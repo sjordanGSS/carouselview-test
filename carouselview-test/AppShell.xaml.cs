a597d8864c7f6a18784eb0c6b604b8dc759e9d38
Sam Jordan <sjordan@gsspos.co.uk>
2024-05-13 09:52:09 +0100

    Add project files.

---
+++ carouselview-test/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace carouselview_test
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ carouselview-test/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace carouselview_test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            IEnumerable<object> items = [string.Empty];
+            carousel.ItemsSource = items;
+        }
+
+        private void Grid_SizeChanged(object sender, EventArgs e)
+        {
+            var grid = (Grid)sender;
+            Console.WriteLine($"Grid size changed to {grid.Width} width and {grid.Height} height");
+        }
+    }
+
+}
